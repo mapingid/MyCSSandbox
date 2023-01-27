a2cf68d9ef8986deb3ec41c71e159286fdcf72e6
Firman Ardiyansyah <firman.ardiyansyah@formulatrix.com>
2023-01-27 11:21:25 +0700

    Delete MainFile, Migrating to Force repo

--- MyCSSandbox/Program.cs
+++ MyCSSandbox/Program.cs
@@ -3,31 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MyCSSandbox
 {
   class Program
   {
-    //static int kuadrat (int i) => i*i ; //lambda expression
     static void Main( string[] args )
     {
-
-      
       
 
-
-
-
-      /*
-      Cartesian p1 = new Cartesian( 3, 0 );
-      Polar p2 = new Polar( 3, 90 );
-      Point2D p3 = Point2D.Resultant( p1, p2 );
-
-      Console.Write( "p1 : " ); p1.Show();
-      Console.Write( "p2 : " ); p2.Show();
-      Console.Write( "p3 : " ); p3.Show();
-      */
       Console.ReadLine();
     }
+
   }
 }
